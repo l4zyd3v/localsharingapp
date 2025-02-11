eca6e943225870661a58aa8906ccd4272929b504
l4zyd3v <nielsen.177@gmail.com>
2025-02-11 15:42:24 +0100

    minor fixes

---
+++ src/scripts/seed.ts
@@ -0,0 +1,67 @@
+export const data: Array<{
+  name: string;
+  extension: string;
+  size: string;
+  date: string;
+}> = [
+  {
+    name: "this-is-a-files",
+    extension: "pdf",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "a-video",
+    extension: "avi",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "textfileshared",
+    extension: "pdf",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "a-video",
+    extension: "avi",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "textfileshared",
+    extension: "pdf",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "this-is-a-files",
+    extension: "pdf",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "a-video",
+    extension: "avi",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "textfileshared",
+    extension: "pdf",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "a-video",
+    extension: "avi",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+  {
+    name: "textfileshared",
+    extension: "pdf",
+    size: "1.2MB",
+    date: "2021-09-01",
+  },
+];
