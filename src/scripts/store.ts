310f5555c2f0b0a814028519bbf724a3b6224469
l4zyd3v <nielsen.177@gmail.com>
2025-02-22 13:26:08 +0100

    upload files functionality

--- src/scripts/store.ts
+++ src/scripts/store.ts
@@ -1,18 +1,18 @@
 let fileStore: File[] = [];
 let subscribers: ((files: File[]) => void)[] = [];
 
-console.log("filestore", fileStore);
-
 export function setFiles(files: File[]): void {
-  console.log("files in function setFiles", files);
-  fileStore = files;
+  fileStore.push(...files);
+  console.log("filestore", fileStore);
   subscribers.forEach((callback) => callback(files));
 }
 
 export function getFiles(): File[] {
+  console.log("getfiles function");
   return fileStore;
 }
 
 export function subscribe(callback: (files: File[]) => void): void {
+  console.log("subscribe function");
   subscribers.push(callback);
 }
